4a3da93acf346ec5be662e25595c0c5fa2d3b787
woojin.cho@kaist.ac.kr <woojin.cho@kaist.ac.kr>
2023-06-28 10:02:04 +0900

    relative path

--- HOnnotate_refine/HOdatasets/mypaths.py
+++ HOnnotate_refine/HOdatasets/mypaths.py
@@ -1,7 +1,7 @@
 import os
 
-# OXR_MULTI_CAMERA_DIR = '/home/uvrlab/projects/HOnnotate_OXR/dataset/'
-#
-# YCB_MODELS_DIR = '/home/uvrlab/projects/HOnnotate_OXR/HOnnotate_refine/YCBobject/models/'
+OXR_MULTI_CAMERA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),  '../../dataset/')
+
+YCB_MODELS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../YCBobject/models/')
 YCB_OBJECT_CORNERS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../objCorners')
 MANO_MODEL_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../optimization/mano/models/MANO_RIGHT.pkl')
