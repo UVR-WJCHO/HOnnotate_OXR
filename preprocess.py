e6e61466d8de5f0945c8ac5a25c3daf9f72c3290
woojin.cho@kaist.ac.kr <woojin.cho@kaist.ac.kr>
2023-09-20 21:32:22 +0900

    .

--- preprocess.py
+++ preprocess.py
@@ -1041,11 +1041,11 @@ def main(argv):
                 target_mp_num = 60
 
             if mp_num_list[-2] < target_mp_num:
-                print("[LOG] seq %s has not enough mediapipe results, try --seq {seq_name} --mp_value 0.9 (or 0.85)"%seqName)
+                print("[!] seq %s has not enough hand results, try with --seq {seq_name} --mp_value 0.9 (or 0.85)"%seqName)
 
         # print("[LOG] total json # in seq %s : %s --- update excel" % (seqName, total_num))
 
-    print("[LOG] total processed time : ", round((time.time() - t0) / 60., 2))
+    print("[log] total processed time : ", round((time.time() - t0) / 60., 2))
 
 
 if __name__ == '__main__':
