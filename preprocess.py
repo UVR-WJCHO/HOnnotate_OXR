b708b25eff04db302eff6fb79437d42f43c23b0b
woojin.cho@kaist.ac.kr <woojin.cho@kaist.ac.kr>
2023-09-25 12:55:15 +0900

    update 4th marker-vert

--- preprocess.py
+++ preprocess.py
@@ -52,7 +52,7 @@ from natsort import natsorted
 
 
 
-flag_check_vert_marker_pair = False
+flag_check_vert_marker_pair = True
 
 ### FLAGS ###
 FLAGS = flags.FLAGS
@@ -486,7 +486,7 @@ class loadDataset():
         if obj_class in CFG_OBJECT_SCALE_SPECIFIC.keys():
             verts_init = verts_init * CFG_OBJECT_SCALE_SPECIFIC[obj_class]
 
-        if verts_init.shape[0] == 3 and obj_class not in CFG_OBJECT_NO4th_POINT:
+        if verts_init.shape[0] == 3 and obj_class in CFG_OBJECT_4th_POINT:
             newpoints_verts_init = np.expand_dims((verts_init[1] + verts_init[2] - verts_init[0]), axis=0)
             newpoints_marker_pose = np.expand_dims((marker_pose[1] + marker_pose[2] - marker_pose[0]), axis=0)
             verts_init = np.concatenate((verts_init, newpoints_verts_init), axis=0)
